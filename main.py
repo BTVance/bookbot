c956c3406d817d3d4c7b3308da8099889ca5bd1a
BTVance <blaketannehill@gmail.com>
2025-04-07 14:08:05 -0700

    passing sys to functions to get book.txt

--- main.py
+++ main.py
@@ -5,13 +5,17 @@ from stats import print_report
 import sys
 def get_book_text(file_path):
     with open(file_path) as f:
-        file_contents = f.read()
+        file_contents = f.read(sys.argv[1])
         return(file_contents)
 
 def main():
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
     get_book_text = get_book_text()
-    word_count = word_in_book()
-    character_count = char_in_book()
+    book_text = get_book_text(sys.argv[1])
+    word_count = word_in_book(book_text)
+    character_count = char_in_book(book_text)
     sorted_chars = sort_chars(character_count)
     print_report(word_count, sorted_chars)
     print("Usage: python3 main.py <path_to_book>")
